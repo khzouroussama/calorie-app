9c4fb082908faad551cf74f8d808b16b38b20b71
Oussama <khzouroussama@gmail.com>
2023-08-15 20:58:50 +0100

    chore: added amplify

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -2,6 +2,17 @@ import { registerRootComponent } from 'expo';
 
 import { App } from './src';
 
+import { Amplify } from 'aws-amplify';
+// import {
+//   Authenticator,
+//   useAuthenticator,
+//   withAuthenticator,
+// } from '@aws-amplify/ui-react-native';
+//
+// import awsExports from './src/aws-exports';
+
+// Amplify.configure(awsExports);
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
